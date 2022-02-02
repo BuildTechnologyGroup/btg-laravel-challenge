cfc44441256f0403e75228d2637c2c8ba7b8e6d8
Zach Christmas <zchristmas@btg.com>
2022-02-02 14:09:30 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Welcome to the BTG Laravel Challenge
+
+This is an exercise to gauge real-world problem solving using PHP, Laravel, and Vue.js
+
+## Getting Started
+
+To get started, download this repository AS A ZIP. Then get your development environment setup. 
+
+A couple of things to links get you started:
+
+- [Laravel Installation](https://laravel.com/docs/8.x/installation)
+- [Node 14 Installation](https://nodejs.org/en/blog/release/v14.4.0/)
+  - [Volta](https://volta.sh/) can be used to manage node installation if you wish
+- [MySQL Installation](https://dev.mysql.com/downloads/installer/)
+
+## The Challenge
+
+1. Get your development environment setup.
+2. Seed data from this laravel project into the correct database/table.
+3. Create API Controller to GET items from the seeded articles table in the laravel mysql database.
+4. Call the created API controller from ExampleComponent.vue and render it in that component.
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+    <br>
+    <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">BTG Laravel Challenge</div>
+
+                    <div class="card-body">
+                        <h4>The Challenge: </h4>
+                        <ol>
+                            <li>Get your development environment setup.</li>
+                            <li>Seed data from this laravel project into the correct database/table.</li>
+                            <li>Create API Controller to GET items from <strong>articles</strong> table in the <strong>laravel</strong> mysql database.</li>
+                            <li>Call the API controller from this component and render it below.</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <hr>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Articles Should Render Below</div>
+
+                    <div class="card-body">
+                        Render Articles Here.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Welcome to the BTG Challenge!.')
+        }
+    }
+</script>
